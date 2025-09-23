d72850744b474107432d3c64c59e1467de09d3f5
Okafor Ebuka <oebuka198@gmail.com>
2025-09-23 22:23:23 +0100

    slide

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
-import {useGSAP} from "@gsap/react"
+ import {useGSAP} from "@gsap/react"
 import gsap from "gsap"
 import { SplitText } from "gsap/all"
+import { useRef } from "react"
+import {useMediaQuery} from "react-responsive"
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767})
+
     useGSAP(()=>{
         //* SPLITTEXT
         const heroSplit = new SplitText('.title', {
@@ -43,6 +49,35 @@ const Hero = () => {
          .to('.left-leaf',{
             y: -200
          }, 0)
+
+         const startValue = isMobile ? 'top 50%' : 'center 60%';
+         const endValue = isMobile ? '120% top' : 'bottom top'
+
+        const tl = gsap.timeline({
+             scrollTrigger:{
+               trigger: 'video',
+               start: startValue,
+               end: endValue,
+               scrub: true,
+               pin: true,
+            }
+         })
+         /* .fromTo(videoRef.current,{
+            currentTime: 0,
+            scale: isMobile ? 1.2 : 1.5,
+            opacity: 0.5
+         },{
+            currentTime: 10,
+            scale: 1,
+            opacity: 1,
+            ease: 'none'
+         }) */
+
+            videoRef.current.onloadedmetadata = ()=>{
+               tl.to(videoRef.current,{
+                  currentTime: videoRef.current.duration
+               })
+            }
     },[])
 
   return (
@@ -71,6 +106,9 @@ const Hero = () => {
         </div>
      </div>
     </section>
+    <div className="video absolute inset-0">{/* ABSOLUTE dont interact with other components on the screen- */}
+        <video ref={videoRef} src="/videos/output.mp4" playsInline preload="auto"></video>
+    </div>
     </>
   )
 }
